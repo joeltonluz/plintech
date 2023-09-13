ec7f35431427d017c2fbcf0a9afe537c5b5b4b5f
Joelton <joelton@in8.com.br>
2023-09-13 18:42:35 -0300

    refactor: findAll menu

--- backend/src/infrastructure/repositories/menu.repositories.ts
+++ backend/src/infrastructure/repositories/menu.repositories.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { MenuRepository } from 'src/domain/repositories';
 import { PrismaService } from '../database/prisma/prisma.service';
-import { MenuM } from 'src/domain/model';
+import { MenuM, ProductM } from 'src/domain/model';
 import { ExceptionsService } from '../exceptions/exceptions.service';
 
+interface IMenu {
+  id: string;
+  period: string;
+  products: ProductM[];
+}
 @Injectable()
 export class DatabaseMenuRepository implements MenuRepository {
   constructor(
@@ -29,22 +34,53 @@ export class DatabaseMenuRepository implements MenuRepository {
     }
 
     return result;
-    // return {
-    //   id: '123',
-    //   period: 'day',
-    //   products: [],
-    // };
   }
 
   async findAll(): Promise<MenuM[]> {
-    const result = await this.prismaService.menu.findMany();
-    const resultMany = await this.prismaService.menu.findMany({
-      include: {
-        products: true,
+    const result: MenuM[] = [];
+
+    const arrayMenus = await this.prismaService.menu.findMany({
+      select: {
+        id: true,
+        period: true,
       },
     });
 
-    console.log('resultmany', resultMany);
+    if (!arrayMenus) return;
+
+    const resultMenus: any[] = [];
+    for (const menu of arrayMenus) {
+      const resultProdInMenu =
+        await this.prismaService.productsInMenus.findMany({
+          where: {
+            menuId: menu.id,
+          },
+          select: {
+            productId: true,
+          },
+        });
+
+      const productList: string[] = [];
+      for (const product of resultProdInMenu) {
+        productList.push(product.productId);
+      }
+      const resultProducts = await this.prismaService.product.findMany({
+        where: {
+          id: { in: productList },
+        },
+        include: {
+          category: true,
+        },
+      });
+
+      resultMenus.push({
+        id: menu.id,
+        period: menu.period,
+        products: [...resultProducts],
+      });
+    }
+
+    result.push(...resultMenus);
 
     return result;
   }
